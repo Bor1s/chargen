3bc75da1efeaaf40203915c7682f5b4d29414664
Boris <boris.bbk@gmail.com>
2017-01-18 21:55:31 +0200

    UI for user actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def active_for(desired_actions = [])
+    if desired_actions.any? { |action| action == "#{controller_name}##{action_name}" }
+      'active'
+    end
+  end
 end
