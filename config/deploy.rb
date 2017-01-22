edba4e5937947f6310f13f33a1dfaf0338a0d6e1
Boris <boris.bbk@gmail.com>
2017-01-22 17:13:21 +0200

    Preparation for deployment p.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :migration_role, :app
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/gandalf/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
