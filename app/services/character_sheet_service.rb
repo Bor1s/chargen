1cf60a338b008ee98379122d5db711c255d244f8
Boris <boris.bbk@gmail.com>
2017-01-29 11:14:07 +0200

    Changes
    - add pdfformfiller java jar to process PDFs
    - FateCore and GeroQuest PDF templates format changes
    - Fate Core sheet UI changes

--- app/services/character_sheet_service.rb
+++ app/services/character_sheet_service.rb
@@ -12,30 +12,46 @@ class CharacterSheetService
     check_if_pdf_template_exist!
 
     @pdf_path        = generate_character_sheet_pdf_name
+    @pdf_tmp_fields_path = generate_character_sheet_pdf_tmp_fields_name(pdf_path)
     @pdftk           = PdfForms.new(ENV['PDFTK_PATH'], data_format: 'XFdf', utf8_fields: true)
   end
 
   def export_to_pdf!
-    fields_hash = fetch_character_sheet_fields_data
-    fill_pdf_form(fields_hash)
+    save_character_sheet_fields_data
+    fill_pdf_form
   end
 
   private
 
-  def fetch_character_sheet_fields_data
+  def save_character_sheet_fields_data
     data = character_sheet.class.stored_attributes[:data_fields].map do |human_field_name|
-      [character_sheet.class::FIELDS_MAP.first[human_field_name], character_sheet.data_fields[human_field_name]]
+      "#{character_sheet.class::FIELDS_MAP.first[human_field_name]} #{escape_unsafe(character_sheet.data_fields[human_field_name])}"
     end
 
-    Hash[data]
+    f = File.open(@pdf_tmp_fields_path, 'w')
+    data.each do |line|
+      f.puts line
+    end
+    f.close
+  end
+
+  def escape_unsafe(string)
+    # TODO convert true/false to 'Yes'/'Off'
+    string&.gsub!(/\\/, '\\\\\\')
+    string&.gsub!(/(\r\n)|(\r)/, '\n')
+    string
   end
 
   def generate_character_sheet_pdf_name
     @base_path.join('tmp', 'downloads', "#{@game_system}_#{SecureRandom.hex(8)}.pdf")
   end
 
-  def fill_pdf_form(data)
-    @pdftk.fill_form @template_path, pdf_path, data
+  def generate_character_sheet_pdf_tmp_fields_name(parent_path_path)
+    @base_path.join('tmp', 'downloads', "#{parent_path_path}_fields.txt")
+  end
+
+  def fill_pdf_form
+    SafeShell.execute("java -jar #{ENV['PDFFILLER_PATH']} #{@template_path} -f #{@pdf_tmp_fields_path} -font #{ENV['PDF_FONT_PATH']} -flatten #{@pdf_path}")
   end
 
   def check_if_pdf_template_exist!
