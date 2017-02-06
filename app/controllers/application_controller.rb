9c70ea55635ed27031a97bed3ab942feff4af592
Boris <boris.bbk@gmail.com>
2017-02-06 23:58:35 +0200

    [WIP]
    - finish revoke link (positioning, help text, spinner)
    - add share button to CharacterSheet Show page
    
    - ensure fate core and hero quest works
    - ensure share work in production host URL
    - ensure share work on mobile devices
    - specs for ShareableLink, ShareableLinkController,
      CreateCharacterSheetService, helpers
    - rake task to fill all empty links for all character sheets
    - refactor JS (fix tooltips, move JS to classes, make Clipboard use only
      input for copying)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    render 'not_found'
+  end
 end
