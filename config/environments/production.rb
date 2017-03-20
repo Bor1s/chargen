83cf509204987e74155d8abde8f7d1b4019d0ad5
Boris <boris.bbk@gmail.com>
2017-03-20 13:59:44 +0200

    Fix smtp mail delivery on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Mailer settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'charactersheets.org@gmail.com',
+    password:             ENV['CS_MAIL_PWD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.default_url_options = { host: 'charactersheets.org' }
 end
